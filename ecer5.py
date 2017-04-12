85ded3fddac3fb93c944e17c6eed07e40382b9c3
lalithbachimanchi <lalithbachimanchi@gmail.com>
2017-04-12 21:16:12 +0530

    India is my country

---
+++ ecer5.py
@@ -0,0 +1,23 @@
+dict={'a':1, 'b':2, 'c':3}
+
+print 'The values in dict are', dict
+dict1={}
+dict1.clear()
+
+
+dict1=dict.copy()
+
+print 'the values after copying', dict1
+
+dict1={'c':2,'d':'3'}
+
+
+print 'the values in dict after updating dict1 are', dict
+print 'the values in dict1 after updation',dict1
+
+
+
+
+
+
+ 
